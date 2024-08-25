1ef2c44550f632a1557ce6debb03dbc3da918498
armannajafib <armannajafi81@gmail.com>
2024-08-25 21:07:11 +0430

    first commit

---
+++ app/src/main/java/com/example/taskmanager/AddEditTaskActivity.kt
@@ -0,0 +1,15 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AddEditTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
